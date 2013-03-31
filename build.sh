1fb6d4317b0e9696b43628fff92d5c1643764f5c
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-03-31 13:09:13 +0200

    Make Mackay kernel - bump version to 1.3
    Change-Id: Ied978cd68528781fa4bf37ae11d31c8308ccb433

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+
+# Build Mackay kernel for p4wifi
+
+# Set basic parameters
+DATE_START=$(date +"%s")
+
+MACKAY_VER="Mackay_1.3"
+
+export ARCH=arm
+export LOCALVERSION="-"`echo $MACKAY_VER`
+export CROSS_COMPILE=/home/kasper/android/cm101/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+
+BASE_DIR=/home/kasper/android/cm101/kernel/samsung
+SOURCE_DIR=`echo $BASE_DIR`/msm7x30
+OUTPUT_DIR=`echo $BASE_DIR`/output/ariesve
+KERNEL_DIR=`echo $OUTPUT_DIR`/kernel
+MODULES_DIR=`echo $OUTPUT_DIR`/modules
+
+# Clean
+rm $KERNEL_DIR/zImage
+rm $MODULES_DIR/*
+
+# Build
+cd $SOURCE_DIR
+make mackay_ariesve_defconfig
+make -j4
+
+# Move modules and kernel to output folder
+# rm `echo $MODULES_DIR"/*"`
+
+find $SOURCE_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+chmod 644 `echo $MODULES_DIR"/*"`
+
+cp arch/arm/boot/zImage $KERNEL_DIR
+
+DATE_END=$(date +"%s")
+echo
+DIFF=$(($DATE_END - $DATE_START))
+echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
